34190fc7ec6ff6269ccc0704619b92cf93327d74
Luis Garcia <waldron.dev@gmail.com>
2024-04-07 03:11:38 -0500

    feat: carousel pagination

--- carousel/viewsets.py
+++ carousel/viewsets.py
@@ -1,11 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, pagination
 from rest_framework.permissions import AllowAny
 from carousel.models import Carousel
 
 from carousel.serializers import CarouselSerializer
 
+class CarouselPagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class CarouselViewSet(viewsets.ModelViewSet):
     queryset = Carousel.objects.all()
     serializer_class = CarouselSerializer
+    pagination_class = CarouselPagination
     permission_classes = [AllowAny]
